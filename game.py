249a0d2bbf203255f937dc1938b3ca3ff243fdd9
hsiachi <hhsiachi@gmail.com>
2023-05-17 01:49:30 +0800

    astar algorithm

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import curses
 from random import randint
+from queue import PriorityQueue
 
 class Snake:
     def __init__(self, window, y, x):
@@ -70,13 +71,14 @@ class Food:
         self.window.addch(self.position[0], self.position[1], curses.ACS_PI)
 
 class SnakeGame:
-    def __init__(self, window, height, width):
+    def __init__(self, window, use_ai, height, width):
         self.window = window
         self.height = height
         self.width = width
         self.score = 0
         self.paused = False
         self.obstacles = []  # List to store obstacle positions
+        self.use_ai = use_ai
 
     def setup(self):
         curses.curs_set(0)
@@ -115,6 +117,115 @@ class SnakeGame:
             return
 
         self.snake.change_direction(next_key)
+
+    def get_valid_directions(self):
+        valid_directions = []
+        head = self.snake.body[0]
+
+        # Check if the snake can move up
+        if self.snake.direction != curses.KEY_DOWN and [head[0] - 1, head[1]] not in self.snake.body:
+            valid_directions.append(curses.KEY_UP)
+
+        # Check if the snake can move down
+        if self.snake.direction != curses.KEY_UP and [head[0] + 1, head[1]] not in self.snake.body:
+            valid_directions.append(curses.KEY_DOWN)
+
+        # Check if the snake can move left
+        if self.snake.direction != curses.KEY_RIGHT and [head[0], head[1] - 1] not in self.snake.body:
+            valid_directions.append(curses.KEY_LEFT)
+
+        # Check if the snake can move right
+        if self.snake.direction != curses.KEY_LEFT and [head[0], head[1] + 1] not in self.snake.body:
+            valid_directions.append(curses.KEY_RIGHT)
+
+        return valid_directions
+
+    def ai_control_snake(self):
+        if not self.food.position or not self.snake.body:
+            return
+
+        # Find the shortest path using A* algorithm
+        path = self.a_star_pathfinding(tuple(self.snake.body[0]), tuple(self.food.position))
+        if path:
+            next_pos = path[1]  # The first position in the path is the current position
+            head = self.snake.body[0]
+            direction = self.get_direction(head, next_pos)
+            self.snake.change_direction(direction)
+
+    def get_direction(self, current_pos, next_pos):
+        y_diff = current_pos[0] - next_pos[0]
+        x_diff = current_pos[1] - next_pos[1]
+
+        if y_diff == 1:  # Move up
+            return curses.KEY_UP
+        elif y_diff == -1:  # Move down
+            return curses.KEY_DOWN
+        elif x_diff == 1:  # Move left
+            return curses.KEY_LEFT
+        elif x_diff == -1:  # Move right
+            return curses.KEY_RIGHT
+
+    def heuristic(self, pos1, pos2):
+        # Calculate the Manhattan distance as the heuristic
+        return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
+
+    def a_star_pathfinding(self, start_pos, goal_pos):
+        open_set = PriorityQueue()
+        open_set.put((0, start_pos))
+        came_from = {}
+        g_score = {start_pos: 0}
+        f_score = {start_pos: self.heuristic(start_pos, goal_pos)}
+
+        while not open_set.empty():
+            current = open_set.get()[1]
+
+            if current == goal_pos:
+                # Reconstruct the path
+                path = [current]
+                while current in came_from:
+                    current = came_from[current]
+                    path.append(current)
+                path.reverse()
+                return path
+
+            neighbors = self.get_neighbors(current)
+            for neighbor in neighbors:
+                temp_g_score = g_score[current] + 1  # Cost of moving to a neighbor is 1
+
+                if neighbor not in g_score or temp_g_score < g_score[neighbor]:
+                    came_from[neighbor] = current
+                    g_score[neighbor] = temp_g_score
+                    f_score[neighbor] = temp_g_score + self.heuristic(neighbor, goal_pos)
+                    open_set.put((f_score[neighbor], neighbor))
+
+        return []
+
+    def get_neighbors(self, position):
+        neighbors = []
+        directions = [
+            (0, -1),  # Left
+            (0, 1),   # Right
+            (-1, 0),  # Up
+            (1, 0)    # Down
+        ]
+
+        for direction in directions:
+            neighbor = (position[0] + direction[0], position[1] + direction[1])
+            if self.is_valid_position(neighbor):
+                neighbors.append(neighbor)
+
+        return neighbors
+
+    def is_valid_position(self, position):
+        # Check if the position is within the game boundaries and not colliding with obstacles or snake body
+        if (
+            position[0] >= 0 and position[0] < self.height and
+            position[1] >= 0 and position[1] < self.width and
+            position not in self.obstacles and
+            position not in self.snake.body
+        ):
+            return True
+        return False
 
     def update(self):
         if self.paused:
@@ -164,6 +275,8 @@ class SnakeGame:
             self.handle_input()
             if not self.update():
                 break
+            if self.use_ai:
+                self.ai_control_snake()
             self.draw()
             self.window.refresh()
 
@@ -183,6 +296,9 @@ class SnakeGame:
         self.game_over_screen()
 
 def get_user_input(stdscr):
+    stdscr.addstr("Use AI or not? (Y/N)")
+    user_input = stdscr.getstr().decode("utf-8")
+    use_ai = user_input.lower() == "y"
     # Get default values
     sh, sw = stdscr.getmaxyx()
     default_height = min(20, sh - 2)
@@ -202,12 +318,12 @@ def get_user_input(stdscr):
     except ValueError:
         pass
 
-    return sh, sw
+    return use_ai, sh, sw
 
 def main(stdscr):
-    sh, sw = get_user_input(stdscr)
+    use_ai, sh, sw = get_user_input(stdscr)
     window = curses.newwin(sh, sw, 0, 0)
-    game = SnakeGame(window, sh, sw)
+    game = SnakeGame(window, use_ai, sh, sw)
     game.run()
 
 curses.wrapper(main)
